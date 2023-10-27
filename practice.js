6e4070558db2484bb131882dea79ab85ddd0e18d
nikoirwin33 <146851989+nikoirwin33@users.noreply.github.com>
2023-10-27 15:40:35 -0400

    FIZZBUZZ

--- practice.js
+++ practice.js
@@ -15,7 +15,12 @@ for(let i = 0; i <=200; i= i+2){
 ////////////////////////////////
 // Fizz Buzz
 ////////////////////////////////
-
+for(let i= 1; i<=100; i++){
+  if(i % 3===0); console.log("Fizz");
+  if(i % 5 ===0); console.log("Buzz")
+  if(i % 3 ===0 && i % 5 === 0) console.log("Fuzz'Buzz");
+  // else console.log(i);
+}
 
 ////////////////////////////////
 // Wild Wild Life
